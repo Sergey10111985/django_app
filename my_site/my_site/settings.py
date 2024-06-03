a4354282f70742653cf648c3cca0683d5e740bcb
sergey <sirotkin.serega1985@yandex.ru>
2024-06-03 21:19:09 +0700

    fix static

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -184,7 +184,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_ROOT = Path(__file__).resolve().parent
+STATIC_ROOT = BASE_DIR
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static'),
 )
